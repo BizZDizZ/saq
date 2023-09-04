3b9930d4fc007d701d817d88063121a371b21965
BizZDizZ <yh6194@naver.com>
2023-09-04 19:12:33 +0900

    first commit

---
+++ js/saq.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    // screensizeCheck();    
+    sliders(".detailslider", false, true, 1, 1, 720, 0);
+    sliders(".discoverslider", false, true, 4, 1, 300, 80);
+    sliders(".carouselSlider", false, true, 4, 1, 400, 60);
+    panelControl("header > div ul li:first-of-type");
+    panelControl("header > div ul li:last-of-type");
+    panelControl("header > div input");
+    panelControl(".newAdd");
+    panelControl(".editAdd");
+    panelControl(".deleteAdd");
+    panelControl(".deleteBtn");
+    panelControl(".payEnd");
+    panelControl(".contactEnd");
+    panelControl(".filterBtn");
+    foldControl(".productContainer #filterPopup > div > ul > li > strong");
+    foldControl(".productContainer .filter > div > ul > li > strong");
+    foldControl(".wineContainer table caption");
+    visibleControl(".vActiveBtn");
+    newvisibleControl(".newvActiveBtn");
+    parentactiveControl("footer > div:last-of-type > div:not(:last-of-type) h3")
+    parentactiveControl(".accountEdit");
+    parentactiveControl(".accountdetailContainer .fillBtn");
+    parentactiveControl(".aboutusContainer.FAQ div .accComponent li h4")
+    permissionCheck();
+    backBtn();
+});
+// function screensizeCheck(){
+//     const mediaQueryList = window.matchMedia('(max-width: 767px)');
+
+//     if(mediaQueryList.matches){
+//         sliders(".carouselSlider", false, true, 1, 1, 280, 0);
+//     }else{
+//         sliders(".carouselSlider", false, true, 4, 1, 185, 60);
+//     }
+//     // mediaQueryList.addEventListener('change', event =>{
+//     //     if(event.matches){
+//     //         sliders(".carouselSlider", false, true, 1, 1, 280, 0);
+//     //     }else{
+//     //         sliders(".carouselSlider", false, true, 4, 1, 185, 60);
+//     //     }
+//     // })
+// }
+function permissionCheck(){
+    $(".before a").click(function(){
+        $(this).attr("href","login.html");
+    });
+}
+function backBtn(){
+    $(".backBtn").click(function(){
+       history.back(); 
+    });
+}
+function sliders(target,indicator,conVal,maxVal,minVal,slideW,slideM){
+    $(target).bxSlider({
+        pager: indicator,
+        controls: conVal,
+        maxSlides: maxVal,
+        minSlides: minVal,
+        slideWidth: slideW,
+        slideMargin: slideM,
+        shrinkItems: true
+    });
+}
+function panelControl(openBtn){
+    var currentPanel = null;
+    $(openBtn).click(function(){
+        if($(this).attr("data-panel")){
+            currentPanel = "#" + $(this).attr("data-panel");
+        }else if($(this).attr("data-popup")){
+            currentPanel = "#" + $(this).attr("data-popup");
+        }
+        $(currentPanel).addClass("active");
+    });
+    $(".closePanel").click(function(){
+        $(currentPanel).removeClass("active");
+    });
+    $(".closePopup").click(function(){
+        $(currentPanel).removeClass("active");
+    });
+    $(".closeBtn").click(function(){
+        $(currentPanel).removeClass("active");
+    });
+}
+function foldControl(target){
+    $(target).click(function(){
+        $(this).toggleClass("active");
+    });
+}
+function parentactiveControl(target){
+    $(target).click(function(){
+        $(this).parent().toggleClass("active");
+    });
+}
+function visibleControl(target){
+    $(target).click(function(){
+        $(".passwordField").toggleClass("active");
+        if($("#userPW").attr("type") == "password"){
+            $("#userPW").attr("type", "text");
+        }else{
+            $("#userPW").attr("type", "password");
+        }
+    });
+}
+function newvisibleControl(target){
+    $(target).click(function(){
+        $(".newpasswordField").toggleClass("active");
+        if($("#newuserPW").attr("type") == "password"){
+            $("#newuserPW").attr("type", "text");
+        }else{
+            $("#newuserPW").attr("type", "password");
+        }
+    });
+}
